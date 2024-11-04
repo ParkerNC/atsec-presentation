f3e27df1ce765650b1177886aab5d101ad733e0c
Parker Collier <parkerncollier@gmail.com>
2024-11-04 15:08:12 -0600

    aes encryption added

--- client.py
+++ client.py
@@ -1,15 +1,20 @@
 
 # echo-client.py
 
+from time import sleep
 from random import randrange
 import socket
-from tools import diffe_helman_decrypt, diffe_helman_pk
+from tools import fastexp
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+from Crypto.Util.Padding import unpad
+from base64 import b64decode
 
 class Client:
     def __init__(self) -> None:
         
         self.HOST = "127.0.0.1"  # The server's hostname or IP address
-        self.PORT = 65434  # The port used by the server
+        self.PORT = 65431  # The port used by the server
         self.p = 13306400456200772160288721147489113378667018293437640666245571086851906965522146421091848113159844868069462038292259294258339777975926320080262975136157147
         self.g = 2
         self.a = randrange(self.p)
@@ -26,22 +31,51 @@ class Client:
         #print(f"Received {num}, {data}")
         return num
 
-    def send_public_key(self, s) -> None:
-        public_key = diffe_helman_pk(self.p, self.a, self.g)
+    def send_public_key(self, s, key) -> None:
         #print(public_key)
-        s.sendall(str(public_key).encode("utf-8"))
-        return public_key
+        s.sendall(str(key).encode("utf-8"))
+    
+    def recieve_message(self, s) -> str:
+        data = s.recv(2048)
+        vals = data.decode("utf-8")
+        vals = vals.split(" ")
+        iv = b64decode(vals[0])
+        ciphertext = b64decode(vals[1])
+        return iv, ciphertext
 
     def connect(self) -> None:
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((self.HOST, self.PORT))
             self.send_params(s)
+            print(f"Sent parameters g:{self.g} \np{self.p}")
+            sleep(2)
+            #recievce servers public key for later use
             server_public = self.recieve_public_key(s)
-            public_key = self.send_public_key(s)
-            key = diffe_helman_decrypt(self.a, server_public, self.p)
-            print(key)         
-
+            print("recieved public key information from server")
+            sleep(2)
+            #send g^a mod p to the server
+            public_key = fastexp(self.g, self.a, self.p)
+            self.send_public_key(s, public_key)
+            print("Calculated g^a (mod p) and sent to server")
+            sleep(2)
+            #calculate the shared key value 
+            key = fastexp(server_public, self.a, self.p)
+            print("Computed shared key value")
+            print(f"Shared Key: {key}")         
+            
+            #convert numeric key into sha hash and use this as key input for aes
+            inp = key.to_bytes((key.bit_length() + 7) // 8, 'big') or b'\0'
+            sharedKey = SHA256.new(inp).digest()[:16]
+            print(f"Hashed key: {sharedKey}")
+            iv, ciphertext = self.recieve_message(s)
+            print("recieved encrypted message from server, decrypting with shared key")
+            print(f"encrypted message: {ciphertext}")
+            sleep(2)
+            cipher = AES.new(sharedKey, AES.MODE_CBC, iv)
+            pt = unpad(cipher.decrypt(ciphertext), AES.block_size)
+            message = pt.decode('utf-8')
+            print(f"message: {message}")
 
             s.close()
     
--- server.py
+++ server.py
@@ -1,14 +1,20 @@
 # echo-server.py
 
+from time import sleep
 import socket
 from random import randrange
-from tools import diffe_helman_decrypt, diffe_helman_pk
+from tools import fastexp
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+from Crypto.Hash import SHA256
+from base64 import b64encode
+
 
 class Server:
     def __init__(self) -> None:
         
         self.HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
-        self.PORT = 65434  # Port to listen on (non-privileged ports are > 1023)
+        self.PORT = 65431  # Port to listen on (non-privileged ports are > 1023)
         self.p = 0
         self.g = 0
         self.a = 0
@@ -21,17 +27,18 @@ class Server:
         self.g = int(vals[1])
         self.a = randrange(self.p)
 
-    def send_public_key(self, conn) -> None:
-        public_key = diffe_helman_pk(self.p, self.a, self.g)
-        conn.sendall(str(public_key).encode("utf-8"))
-        return public_key
+    def send_public_key(self, conn, key) -> None:
+        conn.sendall(str(key).encode("utf-8"))
 
-    def recieve_public_key(self, s) -> int:
-        data = s.recv(2048)
+    def recieve_public_key(self, conn) -> int:
+        data = conn.recv(2048)
         num = int(data.decode("utf-8"))
         #print(f"Received {num}, {data}")
         return num
 
+    def send_encrypted_message(self, conn, iv, ciphertext) -> None:
+        message = f"{iv} {ciphertext}"
+        conn.sendall(message.encode("utf-8"))
 
     def serve(self) -> None:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -41,10 +48,31 @@ class Server:
             with conn:
                 print(f"Connected by {addr}")
                 self.recieve_params(conn)
-                public_key = self.send_public_key(conn)
+                print(f"Recieved Parameters g:{self.g} \np:{self.p}")
+                sleep(2)
+                public_key = fastexp(self.g, self.a, self.p)
+                self.send_public_key(conn, public_key)
+                print("Calculated g^a (mod p) and sent to client")
+                sleep(2)
                 client_public = self.recieve_public_key(conn)
-                key = diffe_helman_decrypt(self.a, client_public, self.p)
-                print(key)
+                print("Recieved public key information from client")
+                sleep(2)
+                key = fastexp(client_public, self.a, self.p)
+                print("Computed shared key value")
+                print(f"Shared Key: {key}")
+
+                #convert numeric key into sha hash and use this as key input for aes
+                inp = key.to_bytes((key.bit_length() + 7) // 8, 'big') or b'\0'
+                sharedKey = SHA256.new(inp).digest()[:16]
+                print(f"Hashed key: {sharedKey}")
+                sleep(2)
+                plaintext = b"atsec information security"
+                cipher = AES.new(sharedKey, AES.MODE_CBC)
+                ciphertext = cipher.encrypt(pad(plaintext, AES.block_size))
+                iv = b64encode(cipher.iv).decode('utf-8')
+                ciphermessage = b64encode(ciphertext).decode('utf-8')
+                self.send_encrypted_message(conn, iv, ciphermessage)
+                print("sent message encrypted with shared key to client")
 
                 conn.close()
 
